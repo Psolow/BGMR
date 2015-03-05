fc8f3ecf47f86cd469493330d26ae5500f62d5f8
Peter Solow <petersolow@gmail.com>
2015-03-05 23:17:08 +0100

    Json

--- Interface/resources/js/modules/module-example.js
+++ Interface/resources/js/modules/module-example.js
@@ -1,6 +1,23 @@
 /*global define:false, Modernizr:false */
 define(['jquery'], function ($) {
     'use strict';
+     $.getJSON('resources/data/vehicles.json', function(data) {
+        var output="<ul>";
+        for (var i in data.vehicles) {
+            var vehicleObject = data.vehicles[i];
+            output+="<li>" + vehicleObject.Name + "</li>";
+            for (var a in vehicleObject.Armorment){
+                output+="<li>" + vehicleObject.Armorment[a] + "</li>";
+            };
+            for (var b in vehicleObject.Ammo){
+                var AmmoObject = vehicleObject.Ammo[b];
+                output+="<li>" + AmmoObject.Name + "</li>";
+                output+="<li>Hit 10CM: " + AmmoObject.Hit[0] + "</li>";
+            }
+            
+        }
 
-    console.log('test module is loaded');
+        output+="</ul>";
+        document.getElementById("placeholder").innerHTML=output;
+  });
 });
